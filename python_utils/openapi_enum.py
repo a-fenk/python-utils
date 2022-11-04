20028a25c174789fe17e65ff3bc106edf5256e45
afenk <artyom.antnv@gmail.com>
2022-11-05 01:57:26 +0200

    openapi_enum

---
+++ python_utils/openapi_enum.py
@@ -0,0 +1,8 @@
+from aenum import Enum
+
+
+class OpenAPIEnum(Enum):
+    @classmethod
+    def __modify_schema__(cls, field_schema: dict):
+        if 'enum' in field_schema:
+            field_schema['enum'] = [{obj.name: obj.value} for obj in cls.__iter__()]
--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open('requirements.txt', 'r') as f:
     requirements = f.read().split('\n')
 
 setuptools.setup(
-    name='sqlalchemy-utils',
+    name='python-utils',
     version='0.0.1',
     author='Tom Antonov',
     author_email='artyom.antnv@gmail.com',
     description='...',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://gitlab.com/skipp1/S954_Skipp3_0/rabbitmq_client',
+    url='https://github.com/a-fenk/python-utils',
     packages=setuptools.find_packages(),
     install_requires=[
 
