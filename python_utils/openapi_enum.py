e5935d616ed90c025d0e4cf7a1072ebd1139d233
afenk <artyom.antnv@gmail.com>
2022-11-11 20:21:23 +0200

    openapi_enum

--- python_utils/openapi_enum.py
+++ python_utils/openapi_enum.py
@@ -5,5 +5,5 @@ class OpenAPIEnum(Enum):
     @classmethod
     def __modify_schema__(cls, field_schema: dict):
         if 'enum' in field_schema:
-            field_schema['enum'] = [obj.value for obj in cls.__iter()]
+            field_schema['enum'] = [obj.value for obj in cls.__iter__()]
             field_schema['enumObj'] = {obj.name: obj.value for obj in cls.__iter__()}
